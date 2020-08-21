c8f6803bc4a2514d36e0632d2b67b119405e6891
ooishujing <50009736+ooishujing@users.noreply.github.com>
2020-08-21 11:16:07 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/demokotlinloop/MainActivity.kt
@@ -0,0 +1,26 @@
+package sg.edu.rp.c346.demokotlinloop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        button.setOnClickListener {
+            for (i in 1..5){
+                Log.d("MainActivity","$i")
+            }
+        }
+
+        button2.setOnClickListener {
+            val word = editText.text.toString()
+            for (letter in word){
+                Log.d("MainActivity","$letter")
+            }
+        }
+    }
+}
